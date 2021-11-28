95a931202917c28da4008c99b832e22e9d031b2c
malmiteria <malmiteria@gmail.com>
2021-11-28 16:07:28 +0100

    model with method to tell what numbers are free on each cell

--- sudoku/models.py
+++ sudoku/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
 def empty_grid():
     return [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -17,7 +16,7 @@ def empty_grid():
     ]
 
 class Sudoku(models.Model):
-    cells = ArrayField(
+    grid = ArrayField(
                 ArrayField(
                     models.IntegerField(blank=True),
                     size=9
@@ -25,3 +24,27 @@ class Sudoku(models.Model):
                 size=9,
                 default=empty_grid
             )
+
+    def possible_number(self, rind, cind):
+        for number in range(1, 10):
+            if number not in self.limiting_cases(rind, cind):
+                yield number
+
+    def limiting_cases(self, rind, cind):
+        yield from self.row(rind)
+        yield from self.col(cind)
+        yield from self.square(rind, cind)
+
+    def row(self, rind):
+        yield from self.grid[rind]
+
+    def col(self, cind):
+        yield from list(zip(*self.grid))[cind]
+
+    def square(self, rind, cind):
+        rowrange = range(3*(rind//3), 3*(rind//3)+3)
+        colrange = range(3*(cind//3), 3*(cind//3)+3)
+        for x in rowrange:
+            for y in colrange:
+                yield self.grid[x][y]
+
--- sudoku/tests.py
+++ sudoku/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
+from .models import Sudoku
 
-# Create your tests here.
+
+class SudokuModelTestCase(TestCase):
+
+    def test_create_sudoku(self):
+        sudoku = Sudoku.objects.create()
+        
+        assert sudoku.grid == [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+    def test_possible_number_on_square_almost_full(self):
+        sudoku = Sudoku.objects.create()
+        sudoku.grid = [
+            [0, 2, 3, 0, 0, 0, 0, 0, 0],
+            [4, 5, 6, 0, 0, 0, 0, 0, 0],
+            [7, 8, 9, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        sudoku.save()
+
+        assert list(sudoku.possible_number(0, 0)) == [1]
+
+    def test_possible_number_on_row_almost_full(self):
+        sudoku = Sudoku.objects.create()
+        sudoku.grid = [
+            [0, 2, 3, 4, 5, 6, 7, 8, 9],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        sudoku.save()
+
+        assert list(sudoku.possible_number(0, 0)) == [1]
+
+    def test_possible_number_on_col_almost_full(self):
+        sudoku = Sudoku.objects.create()
+        sudoku.grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [4, 0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [6, 0, 0, 0, 0, 0, 0, 0, 0],
+            [7, 0, 0, 0, 0, 0, 0, 0, 0],
+            [8, 0, 0, 0, 0, 0, 0, 0, 0],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        sudoku.save()
+
+        assert list(sudoku.possible_number(0, 0)) == [1]
+
