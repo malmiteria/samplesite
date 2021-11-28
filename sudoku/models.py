7475f872456332a8d465a4021864448076f1cffc
malmiteria <malmiteria@gmail.com>
2021-11-28 14:53:37 +0100

    default value on sudoku, and template allowing to fill it in

--- sudoku/models.py
+++ sudoku/models.py
@@ -3,8 +3,25 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+def empty_grid():
+    return [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
 
 class Sudoku(models.Model):
-    cells = ArrayField(ArrayField(models.IntegerField(blank=True), size=9), size=9)
-
-
+    cells = ArrayField(
+                ArrayField(
+                    models.IntegerField(blank=True),
+                    size=9
+                ),
+                size=9,
+                default=empty_grid
+            )
